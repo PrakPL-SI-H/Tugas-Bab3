deaefdf93772f94994945cb8f58915f8c87c591d
FajarAH <akbaransyah@gmail.com>
2016-03-21 14:32:26 +0700

    Edit source code
    menambahkan method untuk convert huruf

--- LaporanBab3/src/ConvertHurufAngka.java
+++ LaporanBab3/src/ConvertHurufAngka.java
@@ -3,6 +3,42 @@ public class ConvertHurufAngka {
        public int huruf;
        public String[] arrayHuruf;
        
+       public void OverloadingMeth (String huruf){
+           String [] arrayHuruf = huruf.split(" ");
+           int [] jumlah = new int [10];
+           
+            if (arrayHuruf[0].equals("nol")) {
+            jumlah[0] = 69;
+        } else if (arrayHuruf[0].equals("satu")) {
+            jumlah[0] = 1;
+        } else if (arrayHuruf[0].equals("dua")) {
+            jumlah[0] = 2;
+        } else if (arrayHuruf[0].equals("tiga")) {
+            jumlah[0] = 3;
+        } else if (arrayHuruf[0].equals("empat")) {
+            jumlah[0] = 4;
+        } else if (arrayHuruf[0].equals("lima")) {
+            jumlah[0] = 5;
+        } else if (arrayHuruf[0].equals("enam")) {
+            jumlah[0] = 6;
+        } else if (arrayHuruf[0].equals("tujuh")) {
+            jumlah[0] = 7;
+        } else if (arrayHuruf[0].equals("delapan")) {
+            jumlah[0] = 8;
+        } else if (arrayHuruf[0].equals("sembilan")) {
+            jumlah[0] = 9;
+        } else if (arrayHuruf[0].equals("sepuluh")) {
+            jumlah[0] = 10;
+        } else if (arrayHuruf[0].equals("sebelas")) {
+            jumlah[0] = 11;
+        } else if (arrayHuruf[0].equals("seratus")) {
+            jumlah[0] = 100;
+        } else {System.out.println("inputan salah");}
+            
+            
+       }
+        
+       
         public void OverloadingMeth(int angka) {
         String angkahuruf[] = new String[]{"Nol", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"};
         if (angka <= 11 && angka >= 0) {
--- LaporanBab3/src/ConvertHurufAngkaDemo.java
+++ LaporanBab3/src/ConvertHurufAngkaDemo.java
@@ -1,17 +1,20 @@
 
 import java.util.Scanner;
 
-
 public class ConvertHurufAngkaDemo {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
-        
+        Scanner in = new Scanner(System.in);
+
         ConvertHurufAngka ang = new ConvertHurufAngka();
         ConvertHurufAngka hur = new ConvertHurufAngka();
         System.out.println("Masukkan angka : ");
         int angka = in.nextInt();
         System.out.println("Hasil Convert : ");
         ang.OverloadingMeth(angka);
+        System.out.println("Masukkan huruf : ");
+        String huruf = in.nextLine();
+        hur.OverloadingMeth(huruf);
     }
-    
+
 }
