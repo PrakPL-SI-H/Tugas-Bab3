de2301423d448ac3cf3bacafde6af3823516371f
FarisGenis <faaris513@gmail.com>
2016-03-23 04:56:06 +0700

    Edit Apotek and main

--- src/Apotek/Apotek.java
+++ src/Apotek/Apotek.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Apotek;
 
-/**
- *
- * @author Faris
- */
 public class Apotek {
-    
+
+    private String nama;
+    private String alamat;
+    private String[] obat = {"Panadol", "Vitamin", "Amoxylin", "Paramex", "Milanta", "Diapet"};
+    private int[] jumlah = new int[20];
+    private int[] harga = {90000, 50000, 30000, 90000, 50000, 30000};
+    private String line = "========================================";
+
+    public Apotek(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+
+    public Apotek(String[] obat, int[] harga) {
+        this.obat = obat;
+        this.harga = harga;
+    }
+
+    public void daftarHarga() {
+        System.out.println(line);
+        System.out.println("tNama Obat \t\t PRICE");
+        for (int i = 0; i < obat.length; i++) {
+            System.out.printf("\t%10s \t %d\n", obat[i], harga[i]);
+        }
+        System.out.println(line);
+    }
+
+    public void daftarHarga(String[] obat, int[] jumlah) {
+        int i, j, k;
+        int l = 0;
+        int TOTAL = 0;
+        System.out.println(line);
+        System.out.println("\tObat \t\t QTY \t PRICE \t TOTAL");
+        for (i = 0; i < obat.length; i++, l++) {
+            if (jumlah[i] == 0) {
+                continue;
+            }
+            for (k = 0; k < obat.length; k++) {
+                int TOT = jumlah[i] + harga[k];
+                System.out.printf("\n\t%-1d  \t%-1s \t %-1d \t %-3d   %-3d", obat[k], jumlah[i], harga[k], TOT);
+                TOTAL += TOT;
+            }
+        }
+        System.out.println(line);
+        System.out.println("\t\t\t\t\t\t\t " + TOTAL);
+    }
 }
--- src/Apotek/Main.java
+++ src/Apotek/Main.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Apotek;
 
-/**
- *
- * @author Faris
- */
+import java.util.Scanner;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        String[] obat = {"Panadol", "Vitamin", "Amoxylin", "Paramex", "Milanta", "Diapet"};
+        String nama, alamat;
+        int[] jumlah = new int[5];
+        int[] harga = {90000, 50000, 30000, 90000, 50000, 30000};
+        String line = "========================================";
+       
+        System.out.println("Program Kasir Apotek");
+        System.out.print("Masukkan nama pasien      : ");
+        nama = in.nextLine();
+        System.out.print("Masukkan alamat pasien    : ");
+        alamat = in.nextLine();
+        
+        Apotek item = new Apotek(obat, harga);
+        Apotek pasien = new Apotek(nama, alamat);
+        
+        item.daftarHarga();
+        
+        int pilih;
+        do {
+            System.out.println("\nMENU:");
+            System.out.println("1. Input obat yang dibeli");
+            System.out.println("2. Print nota");
+            System.out.println("0. Selesai");
+            System.out.print("Pilih Menu : ");
+            pilih = in.nextInt();
+            switch (pilih) {
+                case 1:
+                    input();
+                    break;
+                case 2:
+                    cetak();
+                    break;
+            }
+        } while (pilih != 0);
+    }
+
+    private static void input() {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        System.out.println(line);
+        for (int i = 0; i < 20; i++) {
+            System.out.print("Item Code  " + (i + 1) + "         = ");
+            obat[i] = in.nextInt();
+            if (obat[i] < 1 || obat[i] > 6) {
+                System.out.println("Fail, no entry");
+                System.out.println(line);
+                break;
+            }
+            System.out.print("Qty        " + (i + 1) + "         = ");
+            jumlah[i] = in.nextInt();
+            if (jumlah[i] == 0) {
+                System.out.println(line);
+                break;
+            }
+        }
+    }
 }
