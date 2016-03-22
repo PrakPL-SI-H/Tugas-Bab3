8dbf25b564c8053cd13338008add6ee81f746d30
FarisGenis <faaris513@gmail.com>
2016-03-23 06:15:39 +0700

    Finish all Program

--- src/Apotek/Apotek.java
+++ src/Apotek/Apotek.java
@@ -9,9 +9,7 @@ public class Apotek {
     private int[] harga;
     private String line = "========================================";
 
-    public Apotek(String nama, String alamat) {
-        this.nama = nama;
-        this.alamat = alamat;
+    public Apotek() {
     }
 
     public Apotek(String[] obat, int[] harga) {
@@ -23,28 +21,36 @@ public class Apotek {
         System.out.println(line);
         System.out.println("\t DRUG \t\t PRICE");
         for (int i = 0; i < obat.length; i++) {
-            System.out.printf("\t%10s \t %d\n", obat[i], harga[i]);
+            System.out.printf("\t%s\t\t %d\n", obat[i], harga[i]);
         }
         System.out.println(line);
     }
-
+    
+    public void id(String nama, String alamat){
+        System.out.println("Patient name          : " + nama);
+        System.out.println("Patient address       : " + alamat);
+    }
+    
     public void daftarHarga(String[] obat, int[] jumlah) {
         int i, j, k;
         int l = 0;
         int TOTAL = 0;
+        System.out.println("DRUG \t\t QTY \t PRICE \t TOTAL");
         System.out.println(line);
-        System.out.println("\t DRUG \t\t QTY \t PRICE \t TOTAL");
         for (i = 0; i < obat.length; i++, l++) {
             if (jumlah[i] == 0) {
                 continue;
             }
             for (k = 0; k < obat.length; k++) {
-                int TOT = jumlah[i] + harga[k];
-                System.out.printf("\n\t%-1d  \t%-1s \t %-1d \t %-3d   %-3d", obat[k], jumlah[i], harga[k], TOT);
-                TOTAL += TOT;
+                if (obat[l].equalsIgnoreCase(this.obat[k])) {
+                    int TOT = jumlah[i] * harga[k];
+                    System.out.printf("%s\t\t %d\t %d\t %d", this.obat[k], jumlah[i], harga[k], TOT);
+                    TOTAL += TOT;
+                }
             }
         }
+        System.out.println("\n"+line);
+        System.out.println("\t\t\t\t " + TOTAL);
         System.out.println(line);
-        System.out.println("\t\t\t\t\t\t\t " + TOTAL);
     }
 }
