a09bff4152d1a5084ea10a16522c7c4ecfe9a5b9
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-22 11:33:14 +0700

    inputan nama dan alamat

---
+++ src/tugas/bab3/Apotek.java
@@ -0,0 +1,14 @@
+package prakproglan;
+import java.util.Scanner;
+public class Apotek {
+    
+    public static void main(String[] args) {
+        String nama, alamat;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Nama : ");
+        nama = in.nextLine();
+        System.out.print("Alamat : ");
+        alamat = in.nextLine();
+    }
+}
