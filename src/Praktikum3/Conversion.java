cbd4ee0a8c9cfae4bfc8befea7b4dc5dbc2b033c
FarisGenis <faaris513@gmail.com>
2016-03-23 01:05:54 +0700

    add number2Words method

--- src/Praktikum3/Conversion.java
+++ src/Praktikum3/Conversion.java
@@ -9,23 +9,37 @@ public class Conversion {
         for (int i = 0; i < this.words.length; i++) {
             for (int j = 0; j < this.words.length; j++) {
                 if (words.equalsIgnoreCase(this.words[i] + " Puluh " + this.words[j])) {
-                    System.out.println("In words : " + (i) + (j));
+                    System.out.println("In number : " + (i) + (j));
                 }
             }
 
             if (words.equalsIgnoreCase(this.words[13])) {
-                System.out.println("In words        : " + this.words[12]);
+                System.out.println("In number        : " + this.words[12]);
             } else if (words.equalsIgnoreCase(this.words[i])) {
-                System.out.println("In words        : " + i);
+                System.out.println("In number        : " + i);
             } else if (words.equalsIgnoreCase(this.words[i] + "Belas")) {
-                System.out.println("In words        : " + (i + 10));
+                System.out.println("In number        : " + (i + 10));
             } else if (words.equalsIgnoreCase(this.words[i] + "Puluh")) {
-                System.out.println("In words        : " + (i * 10));
-            } else if (words.equalsIgnoreCase(this.words[i])) {
-
+                System.out.println("In number        : " + (i * 10));
+            } else {
+                System.out.println("Input out of range");
             }
         }
     }
-    
-    
+  
+    public void number2Words(int num) {
+        if (num == 0) {
+            System.out.println("In words        : " + words[13]);
+        } else if (num <= 11){
+            System.out.println("In words        : " + words[num]);
+        } else if (num <= 19){
+            System.out.println("In words        : " + words[num - 10] + " Belas");
+        } else if (num < 100){
+            System.out.println("In words        : " + words[num / 10] + " Puluh " + words[num % 10]);
+        } else if (num == 100){
+            System.out.println("In words        : " + words[12]);
+        } else {
+            System.out.println("Input out of range");
+        }
+    }
 }
