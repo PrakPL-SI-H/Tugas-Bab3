4f241ae0bb6f7101c24d3f7eddcdea8f077be78c
FajarAH <akbaransyah@gmail.com>
2016-03-22 13:36:49 +0700

    Edit source code program 2
    membuat pilihan untuk pembeli

--- LaporanBab3/src/ApotekDemo.java
+++ LaporanBab3/src/ApotekDemo.java
@@ -2,7 +2,9 @@
 import java.util.Scanner;
 
 public class ApotekDemo {
+    public static int pilihan;
     public static void main(String[] args) {
+        int banyak,pil;
         Scanner in = new Scanner (System.in);
         System.out.println("Masukkan Nama Anda : ");
         String nama = in.nextLine();
@@ -10,6 +12,18 @@ public class ApotekDemo {
         String alamat = in.nextLine();
         Pembeli pasien = new Pembeli (nama,alamat);
         Obat pasien1 = new Obat();
-        pasien1.displayMessage();
+        
+        
+        do{
+            pasien1.displayMessage();
+            System.out.println("Masukkan pilihan anda : ");
+            pilihan = in.nextInt();
+            System.out.println("Masukkan berapa unit obat yang akan anda beli : ");
+            banyak = in.nextInt();
+            System.out.println("");
+            Pembeli obat = new Pembeli (pilihan,banyak);
+            pil = in.nextInt();
+        
+        }while(pil != 2);
 }
 }
--- LaporanBab3/src/Pembeli.java
+++ LaporanBab3/src/Pembeli.java
@@ -7,4 +7,36 @@ public class Pembeli {
         this.nama = nama;
         this.alamat = alamat;
     }
+    Pembeli (int pilihan, int banyak){
+       switch (pilihan){
+           case 1 : 
+               System.out.printf("|%-5s |%-30s |%-10s | \n", "No", "Nama Obat", "Harga");
+                System.out.printf("|%-5s |%-30s |%10s | \n", "1", "CALAMINT LOTION 100 ML", "Rp.  9.400");
+                System.out.println("Banyaknya obat yang akan anda beli\t: " + banyak);
+               break;
+           case 2 : 
+                System.out.printf("|%-5s |%-30s |%-10s | \n", "No", "Nama Obat", "Harga");
+                System.out.printf("|%-5s |%-30s |%5s | \n", "2", "CALCIDOL SIRUP 120 ML", "Rp. 34.900");
+                System.out.println("Banyaknya obat yang akan anda beli\t: " + banyak);
+               break;
+           case 3 : 
+                System.out.printf("|%-5s |%-30s |%-10s | \n", "No", "Nama Obat", "Harga");
+                System.out.printf("|%-5s |%-30s |%5s | \n", "3", "CAPTOPRIL 50 MG", "Rp. 11.900");
+                System.out.println("Banyaknya obat yang akan anda beli\t: " + banyak);
+               break;
+           case 4 : 
+                System.out.printf("|%-5s |%-30s |%-10s | \n", "No", "Nama Obat", "Harga");
+                System.out.printf("|%-5s |%-30s |%5s | \n", "4", "CHLORAMPHECORT CREAM 10 G", "Rp. 18.350");
+                System.out.println("Banyaknya obat yang akan anda beli\t: " + banyak);
+               break;
+           case 5 : 
+               System.out.printf("|%-5s |%-30s |%-10s | \n", "No", "Nama Obat", "Harga");
+                System.out.printf("|%-5s |%-30s |%5s | \n", "5", "SCABICID CREAM 10 G", "Rp. 38.500");
+                System.out.println("Banyaknya obat yang akan anda beli\t: " + banyak);
+               break;
+           default : 
+               System.out.println("Pilihan yang anda masukkan salah, silahkan coba lagi");
+       }
+    
+    }
 }
