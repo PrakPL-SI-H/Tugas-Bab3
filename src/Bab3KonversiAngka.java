0314bf0b4f59c031b2f92870bc860b6986302727
istighfarinba <bafarcorp@gmail.com>
2016-03-23 18:14:02 +0700

    meneruskan main class(pembuatan menu program) dan proses pemanggilan beberapa method dari class ProsesKonversi dengan objek

--- src/Bab3KonversiAngka.java
+++ src/Bab3KonversiAngka.java
@@ -1,6 +1,6 @@
 package bab3konversiangka;
 
-
+import java.util.Scanner;
 
 /**
  *
@@ -11,7 +11,37 @@ package bab3konversiangka;
 public class Bab3KonversiAngka {
 
     public static void main(String[] args) {
-       
+        Scanner in = new Scanner(System.in);
+        ProsesKonversi convert = new ProsesKonversi();
+        String loop;
+        int pil;
+        
+        do {
+            System.out.println("");
+            System.out.print("    SILAHKAN PILIH MENU : ");
+            pil = in.nextInt();
+            switch (pil) {
+                case 1:
+                    System.out.print("Masukkan huruf anda    : ");
+                    in.nextLine();
+                    convert.overLoadingMeth(in.nextLine());
+                    convert.outputnum();
+                    break;
 
+                case 2:
+
+                    System.out.print("Masukkan angka anda    : ");
+                    int num = in.nextInt();
+                    convert.overloadingMeth(num);
+                    convert.outputword();
+                    break;
+
+            }
+            System.out.print("ULANGI LAGI ?(Y/N)");
+            loop = in.next();
+        } while ("Y".equals(loop));
+        System.out.println("");
+        System.out.println("     ===PROGRAM CLOSED===");
+        System.out.println("         TERIMA KASIH    ");
     }
 }
