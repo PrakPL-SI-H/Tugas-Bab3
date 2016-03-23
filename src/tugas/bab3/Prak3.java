1f101a4980ec7bcdc456293fbbe3b4797734f83a
wenigustirahayu <wenigr97@gmail.com>
2016-03-23 18:36:21 +0700

    Penyelesaian main class OverloadingMeth

--- src/tugas/bab3/Prak3.java
+++ src/tugas/bab3/Prak3.java
@@ -4,6 +4,15 @@ public class Prak3 {
 public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
         Scanner in2 = new Scanner (System.in);
+        OverloadingMeth o = new OverloadingMeth();
+        System.out.print("Input angka (1-100) : ");
+        int angka = in.nextInt();
+        System.out.print("Huruf : ");
+        o.overloadingMeth(angka);
+        System.out.println(" ");
+        System.out.print("Input angka dalam huruf (Satu-Seratus) : ");
+        String hur = in2.nextLine();
+        System.out.println("Angka : "+o.overloadingMeth(hur));
         
     }
 }	
