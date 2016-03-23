8ea7786e4fe4410f4d73bc6d2033a2e2cccff324
wenigustirahayu <wenigr97@gmail.com>
2016-03-23 19:44:00 +0700

    Edit class Apotek dengan pemberian seleksi kondisi pemilihan obat

--- src/tugas/bab3/Apotek.java
+++ src/tugas/bab3/Apotek.java
@@ -9,3 +9,41 @@ public class Apotek {
         this.nama = nama;
         this.alamat = alamat;
     }
+
+    public Apotek(int a, int b) {
+        this.menu = a;
+        this.banyak = b;
+        if (menu == 1) {
+            System.out.println("Paracetamol 5000 x " + banyak);
+            HitungTotal(5000, banyak);
+        } else if (menu == 2) {
+            System.out.println("Decolgen 2000 x " + banyak);
+            HitungTotal(2000, banyak);
+        } else if (menu == 3) {
+            System.out.println("Combantrin 25000 x " + banyak);
+            HitungTotal(25000, banyak);
+        } else if (menu == 4) {
+            System.out.println("Antalgin 4500 x " + banyak);
+            HitungTotal(4500, banyak);
+        } else if (menu == 5) {
+            System.out.println("Amoxilin 10000 x " + banyak);
+            HitungTotal(10000, banyak);
+        } else if (menu == 6) {
+            System.out.println("Inzana 3000 x " + banyak);
+            HitungTotal(3000, banyak);
+        } else if (menu == 7) {
+            System.out.println("Tolak Angin 7500 x " + banyak);
+            HitungTotal(7500, banyak);
+        } else if (menu == 8) {
+            System.out.println("Bodrex  5500 x " + banyak);
+            HitungTotal(5500, banyak);
+        } else if (menu == 9) {
+            System.out.println("Betadine 13000 x " + banyak);
+            HitungTotal(13000, banyak);
+        } else if (menu == 10) {
+            System.out.println("Minyak Kayu Putih 9000 x " + banyak);
+            HitungTotal(9000, banyak);
+        } else {
+            System.out.println("Pilihan Anda Tidak Ditemukan");
+        }
+    }
