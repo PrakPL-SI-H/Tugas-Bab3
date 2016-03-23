d9e04dd188b52a8c41716ec3394b9cf898723009
FelixYosa <felix_yosa@ymail.com>
2016-03-23 22:32:18 +0700

    edit apotek

--- src/tugas/bab3/Apotek.java
+++ src/tugas/bab3/Apotek.java
@@ -3,23 +3,32 @@ package mainapotek;
 public class Apotek {
 
     public String alamat, nama;
-    public String kode;
     private int harga = 0;
-    public int jumlah;
+    public int jumlah[];
 
     Obat list = new Obat();
-    
+
     public void setData(String nama, String alamat) {
         this.nama = nama;
         this.alamat = alamat;
     }
 
-    public void setData(String kode, int j) {
-        this.kode = kode;
-        for(int i=0;i<5;i++){
-            
+    public void setData(int a, int b) {
+        for (int i = 0; i < 5; i++) {
+            if (a == list.kode[i]) {
+                jumlah[i] = list.uang[i] * b;
+                harga = harga + jumlah[i];
+            }
         }
-        
+    }
+
+    public void beli() {
+        for (int i = 0; i < 5; i++) {
+            if (jumlah[i] != 0) {
+                System.out.println(list.kode[i] + "     |  " + list.obat[i]
+                        + "       |  " + jumlah[i]);
+            }
+        }
     }
 
     public int getHarga() {
