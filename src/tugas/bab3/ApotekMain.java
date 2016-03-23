33241f903492cdaadc1490203bce8a0a8fe5cfd9
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-03-23 22:19:48 +0700

    percobaan pertama class apotekmain,

---
+++ src/tugas/bab3/ApotekMain.java
@@ -0,0 +1,34 @@
+package program_Laporan;
+
+import java.util.Scanner;
+
+
+public class ApotekMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        ClassObat drug=new ClassObat();
+       
+        int jumlah,jumlah2,jumlah3,jumlah4,jumlahsemua = 0;
+        int harga ;
+        int total;
+        int TotalSemua = 0;
+        String answer;
+        System.out.println("===|||SELAMAT DATANG DI APOTEK SIANIDAFARMA|||===");
+        System.out.println();
+        System.out.println("gudangnya obat masalah duniawi");
+        
+        System.out.println("silahkan menginputkan nama anda:" );
+        String nama=in.next();
+        in.next();
+        System.out.println("Silahkan menginputkan alamat anda: ") ;
+        String alamat=in.next();
+        in.next();
+       
+        
+}
+}
+
