cb0fcf2543a5a53bcf5aa4cfe940d80d673be938
istighfarinba <bafarcorp@gmail.com>
2016-03-23 18:27:23 +0700

    deklarasi variabel baru pada class DataApotik

--- src/Program Apotik/DataApotik.java
+++ src/Program Apotik/DataApotik.java
@@ -2,6 +2,11 @@ package bab3sistemapotik;
 
 public class DataApotik {
 
+    public String nama, user, alamat, obat;
+    public int[] harga;
+    public String[] list, pilObat;
+    public int jum, total;
+
    
 
 }
