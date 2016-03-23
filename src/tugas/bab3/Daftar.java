d17e2ae2f78e469a458dfd4b479fd71c220040da
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-23 21:11:24 +0700

    edit method class soal no2

---
+++ src/tugas/bab3/Daftar.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Daftar {
+
+private int harga;
+private int tmb;
+private int jumlah;
+public int total;
+public String Alamat;
+public String Nama;
+public int pilih;
+
+Scanner in = new Scanner(System.in);
+public void menu(){
+    System.out.println("Menu");
+    System.out.println("No Nama Obat            Harga ");
+    System.out.println("1. Obat Sakit Kepala   Rp 5000");
+    System.out.println("2. Obat Sakit Gigi     Rp 6000");
+    System.out.println("3. Obat Sakit Flu      Rp 7000");
+    System.out.println("4. Obat Sakit Panas    Rp 6500");
+    System.out.println("5. Obat Sakit Pusing   Rp 5000");
+    System.out.println("6. Obat Pegel Linu     Rp 10000");
+}
+    public String Alamat(){
+    return Alamat;
+}
+    public String Nama(){
+    return Alamat;
+}
+
+}
