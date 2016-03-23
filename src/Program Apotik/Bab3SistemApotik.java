3851efd7994f3929d05be618c89d6c2729a025e2
istighfarinba <bafarcorp@gmail.com>
2016-03-23 18:32:43 +0700

    pembuatan array daftar obat dan harganya

--- src/Program Apotik/Bab3SistemApotik.java
+++ src/Program Apotik/Bab3SistemApotik.java
@@ -12,6 +12,14 @@ public class Bab3SistemApotik {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int pil;
+        String lagi;
+        String loop;
+        int beliObat[];
+        int jumObat[];
+        String daftar[] = {"ObatH5N1", "ObatH222", "Obat5W1H", "ObatK2K3", "ObatXXXX"};
+        int price[] = {10000, 20000, 30000, 40000, 50000};
+
         
 
 }}
