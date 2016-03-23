0fe82400049a947531706bfb93458aaa61ca14aa
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-03-23 22:17:16 +0700

    percobaan kedua class obat dengan 2 method

--- src/tugas/bab3/ClassObat.java
+++ src/tugas/bab3/ClassObat.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 public class ClassObat {
      Scanner in = new Scanner(System.in);
      int totalObat1,totalObat2,totalObat3,totalObat4;
+      public void Obat1(int jumlah){
+         
+       System.out.println("jumlah=" +jumlah);
+         totalObat1=(jumlah*10000);
+        System.out.println("Harganya adalah ="+totalObat1);
+        
+   }
+     public void Obat2(int jumlah2){
+       
+       System.out.println("jumlah=" +jumlah2);
+          totalObat2=(jumlah2*20000);
+        System.out.println("Harganya adalah ="+totalObat2);
+         
+   }
      
        public void totalAll(int jumlahsemua){
           
