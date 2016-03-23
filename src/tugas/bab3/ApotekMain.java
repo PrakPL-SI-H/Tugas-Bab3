c33e34339d56f5e57bdc933bba7e7f40b9c9e696
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-03-23 22:23:28 +0700

    percobaan kedua class apotekmain dengan 3 case utama

--- src/tugas/bab3/ApotekMain.java
+++ src/tugas/bab3/ApotekMain.java
@@ -27,7 +27,59 @@ public class ApotekMain {
         System.out.println("Silahkan menginputkan alamat anda: ") ;
         String alamat=in.next();
         in.next();
-       
+        do{
+        System.out.println("=============DAFTAR OBAT DI APOTEK SIANIDAFARMA================");
+        System.out.println("1. Chefalogrin(Obat pusing tugas) ");
+        System.out.println("2. Hypertaskosin(Obat darah tinggi kebanyakan tugas) ");
+        System.out.println("3. Ampegrin(Obat sesak kena revisi)");
+        System.out.println("4. Eynofarmin(obat mata pedas kebanyakan ngoding)");
+        System.out.println("5.Saya ingin melihat harga total pembelian saya");
+        System.out.println("6. Keluar");
+        System.out.print("Masukkan pilihan Anda: ");
+        int pilih = in.nextInt();
+       switch (pilih){
+            case 1:
+          System.out.println("====Chefalogrin(Obat pusing tugas)====");
+        System.out.println("Harga per botol adalah 10000 rupiah");
+         System.out.println("Masukkan berapa botol yang akan anda beli");   
+         
+         
+         System.out.println("Banyak obat(botol)   = ");
+          
+         jumlah = in.nextInt();
+          drug.Obat1(jumlah);
+               break;
+                
+            case 2:
+                System.out.println("====Hypertaskosin(Obat darah tinggi kebanyakan tugas)====");
+        System.out.println("Harga per botol adalah 20000 rupiah");
+         System.out.println("Masukkan berapa botol yang akan anda beli");
+                
+                System.out.println("Banyak obat(botol)   = ");
+               
+         jumlah2 = in.nextInt();
+         drug.Obat2(jumlah2);
+                break;
+            case 3:
+                 System.out.println("====Ampegrin(Obat sesak kena revisi)====");
+        System.out.println("Harga per botol adalah 25000 rupiah");
+         System.out.println("Masukkan berapa botol yang akan anda beli");
+         
+    
+               
+                System.out.println("Banyak obat(botol)   = ");
+                
+         jumlah3 = in.nextInt();
+           drug.Obat3(jumlah3);
+                break;
+           
+            default:
+                System.out.println("Mohon Maaf, Pilihan anda Tidak Ditemukan");      
+        }
+        
+        System.out.print("Melanjutkan Pembelian obat? (Y/N) ");
+        answer = in.next();
+    }while(answer.equalsIgnoreCase("Y"));
         
 }
 }
