401e985f96024d99ef175e5d7202a39146ba28a6
Johan Fajar <johanrubik@gmail.com>
2016-03-23 19:33:58 +0700

    edit mainApotek
    hy hy

--- src/tugas/bab3/mainApotek/src/mainapotek/MainApotek.java
+++ src/tugas/bab3/mainApotek/src/mainapotek/MainApotek.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mainapotek;
 
-/**
- *
- * @author ASUS-A455L
- */
+import java.util.Scanner;
+
 public class MainApotek {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Sistem Informasi Penjualan Apotek");
+        System.out.println("Masukkan nama : ");
+        String a = in.nextLine();
+        System.out.println("Masukkan alamat : ");
+        String b = in.nextLine();
+        
+        
     }
     
 }
