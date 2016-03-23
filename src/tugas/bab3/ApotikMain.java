851b5c499875081c1ac4d8dc9f8d182b42d8bfb8
arifardhan <ariframadhan902@yahoo.com>
2016-03-23 21:32:26 +0700

    Membuat switch case pada menu program

--- src/tugas/bab3/ApotikMain.java
+++ src/tugas/bab3/ApotikMain.java
@@ -10,7 +10,7 @@ public class ApotikMain {
     static String nama, alamat;
     static int[] jumlah = new int[5];
     static int[] harga = {4000, 15000, 5000, 5000, 12000};
-    
+
     public static void main(String[] args) {
         System.out.println("SELAMAT DATANG DI APOTEK AMANAH");
         System.out.println("-------------------------------");
@@ -27,5 +27,29 @@ public class ApotikMain {
         item.daftarHarga();
 
         int pilih;
-
+        do {
+            System.out.println("SELAMAT DATANG DI APOTEK AMANAH");
+            System.out.println("-------------------------------");
+            System.out.println("     JL.MT.HARYONO NO.264      ");
+            System.out.println("===============================");
+            System.out.println("1. Input obat");
+            System.out.println("2. Cetak nota");
+            System.out.println("0. Keluar");
+            System.out.print("Masukkan Pilihan : ");
+            pilih = in.nextInt();
+            switch (pilih) {
+                case 1:
+                    input();
+                    break;
+                case 2:
+                    System.out.println(line);
+                    System.out.println("SUMMARY");
+                    System.out.println(line);
+                    pasien.id(nama, alamat);
+                    System.out.println(line);
+                    item.daftarHarga(inputObat, jumlah);
+                    break;
+            }
+        } while (pilih != 0);
+    }
 }
