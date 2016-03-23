5f7944e2558a8eb44da8ba0a0689ec42da5b94ba
istighfarinba <bafarcorp@gmail.com>
2016-03-23 18:39:29 +0700

    penyelesaian case..

--- src/Program Apotik/Bab3SistemApotik.java
+++ src/Program Apotik/Bab3SistemApotik.java
@@ -13,10 +13,7 @@ public class Bab3SistemApotik {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int pil;
-        String lagi;
         String loop;
-        int beliObat[];
-        int jumObat[];
         String daftar[] = {"ObatH5N1", "ObatH222", "Obat5W1H", "ObatK2K3", "ObatXXXX"};
         int price[] = {10000, 20000, 30000, 40000, 50000};
 
@@ -29,14 +26,12 @@ public class Bab3SistemApotik {
 
         DataApotik user = new DataApotik(nama, alamat); //overloading construktor
         DataApotik list = new DataApotik(daftar, price);
-        System.out.println("===SELAMAT DATANG DI APOTIK BAHTIAR===");
-        System.out.println("");
-        do {
-            menu();
 
+        menu();
+        do {
             System.out.println();
             System.out.print("SILAHKAN PILIH MENU : ");
-
+            
             pil = in.nextInt();
             switch (pil) {
                 case 1:
@@ -46,6 +41,30 @@ public class Bab3SistemApotik {
 
                     }
                     break;
-                
+                case 2:
+                    System.out.println("===PEMBELIAN OBAT===");
+                    System.out.print("Masukkan Nama Obat   : ");
+                    String namaObat = in.next();
+                    System.out.print("masukkan Jumlah Obat : ");
+                    int jum = in.nextInt();
+                    list.olahData(namaObat, jum);
+                    break;
+
+                case 3:
+                    System.out.println("            ===APOTIK BAHTIAR===");
+                    System.out.println("    =====================================");
+                    System.out.println("   Nama Pembeli     :" + user.user);
+                    System.out.println("   Alamat Rumah     :" + user.alamat);
+                   
+                    list.outputProgram();
+                    break;
+                case 4:
+                    break;
+            }
+
+        } while (pil != 4);
+        System.out.println("===APOTIK BAHTIAR===");
+        System.out.println("   TERIMA KASIH");
+    }
 
 }
