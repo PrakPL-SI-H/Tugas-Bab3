958f7a04defb84c474658cfc6fa54b2bfff18417
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-23 20:55:11 +0700

    membuat main dari BilanganTeks

---
+++ src/tugas/bab3/MainBilanganTeks.java
@@ -0,0 +1,14 @@
+package prakproglan;
+import java.util.Scanner;
+public class MainBilanganTeks {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BilanganTeks bete = new BilanganTeks();
+        System.out.println("Pilihan Ubah : ");
+        System.out.println("1. Mengubah Bilangan Menjadi Teks");
+        System.out.println("2. Mengubah Teks Menjadi Bilangan");
+        System.out.print("Masukkan Pilihan : ");
+        int pilihan = in.nextInt();
+        
+    }
+}
