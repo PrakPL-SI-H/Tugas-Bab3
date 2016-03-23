e1bf238465219f94bf27b36a69e11e0f0f08dad5
rodhirf <rodhirobb@gmail.com>
2016-03-23 07:49:36 +0700

    membuat method dan array

--- src/tugas/bab3/TugasBab3.java
+++ src/tugas/bab3/TugasBab3.java
@@ -3,3 +3,36 @@ import java.util.Scanner;
 public class PraktikumBab3 {
 int satuan, hasilsatuan;
 String bilangan, hasilbilangan;
+
+public PraktikumBab3() {
+}
+public PraktikumBab3(String x) {
+bilangan = x;
+}
+String[] bilangans = {"", "satu", "dua", "tiga", "empat", "lima", "enam","tujuh", "delapan", "sembilan", "sepuluh", "sebelas"};
+String[] bilanganspuluhan = {"","sepuluh","dua puluh ","tiga puluh ","empat puluh ","lima puluh ","enam puluh ","tujuh puluh ","delapan puluh ","sembilan puluh "};
+public PraktikumBab3(int y) {
+satuan = y;
+}
+int[] satuans = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+public void overloadingMeth(String x) {
+if (x.equalsIgnoreCase("seratus")) {
+hasilsatuan = 100;
+} else {
+for (int a = 0; a < bilangans.length; a++) {
+for (int b = 0; b < bilangans.length; b++) {
+if (x.equalsIgnoreCase(bilangans[a])) {
+hasilsatuan = a;
+} else if (x.equalsIgnoreCase(bilangans[a] + " puluh")) {
+hasilsatuan = a * 10;
+} else if (x.equalsIgnoreCase(bilangans[a] + " belas")) {
+hasilsatuan = a + 10;
+} else if (x.equalsIgnoreCase(bilangans[a] + " puluh " + bilangans[b])) {
+hasilsatuan = a * 10 + b;
+}
+}
+}
+}
+System.out.println("Outpunya adalah "+hasilsatuan);
+}
+
