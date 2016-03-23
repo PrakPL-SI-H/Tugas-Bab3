9c93e4852f67031fbefb65bea4663d65a53ecb6d
LiliKordina13 <lilikordinana98@gmail.com>
2016-03-23 20:20:26 +0700

    Perbaikan Public Class Obat (Soal 2)

--- src/tugas/bab3/Public Class Obat (Soal 2).java
+++ src/tugas/bab3/Public Class Obat (Soal 2).java
@@ -1,31 +1,62 @@
+package apotek;
+
+import java.util.Scanner;
 
 public class Obat {
+
     public String obat[];
     public int harga;
+
     Obat() {
-        this.obat = new String[2];
+        this.obat = new String[9];
     }
+
     public String[] obat(int pilih) {
         if (pilih == 1) {
-            obat[0] = "generic";
+            obat[0] = "Antangin";
         } else if (pilih == 2) {
-            obat[1] = "abal";
+            obat[1] = "VitalongC";
+        } else if (pilih == 3) {
+            obat[2] = "EsterC";
+        } else if (pilih == 4) {
+            obat[3] = "TolakAngin";
+        } else if (pilih == 5) {
+            obat[4] = "Bodrek";
+        } else if (pilih == 6) {
+            obat[5] = "Inzana";
+        } else if (pilih == 7) {
+            obat[6] = "Termorex";
+        } else if (pilih == 8) {
+            obat[7] = "Paracetamol";
+        } else if (pilih == 9) {
+            obat[8] = "Mixagrip";
         }
         return obat;
     }
+
     public int harga(int pilihan) {
         if (pilihan == 1) {
-            harga += 10000;
+            harga += 5000;
         } else if (pilihan == 2) {
             harga += 20000;
+        } else if (pilihan == 3) {
+            harga += 30000;
+        } else if (pilihan == 4) {
+            harga += 5000;
+        } else if (pilihan == 5) {
+            harga += 3000;
+        } else if (pilihan == 6) {
+            harga += 2000;
+        } else if (pilihan == 7) {
+            harga += 10000;
+        } else if (pilihan == 8) {
+            harga += 15000;
+        } else if (pilihan == 9) {
+            harga += 3000;
         }
         return harga;
     }
+
     public void display() {
-        for (int i = 0; i < obat.length; i++) {
-            System.out.println("Obat " + (i + 1) + " : " + obat[i]);
-        }
-        System.out.println("Harga : " + harga);
+        System.out.println("Jumlah harga yang harus dibayar : " + harga);
     }
-    
-}
