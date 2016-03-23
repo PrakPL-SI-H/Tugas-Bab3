0d0e9489ce518930b5dc5188e1ea3b96002b0833
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-23 23:02:16 +0700

    menambahkan method obat1-obat5 dan jumlahobat

--- src/tugas/bab3/Apotek.java
+++ src/tugas/bab3/Apotek.java
@@ -6,6 +6,7 @@ public class Apotek {
         String obat;
         int harga;
         int total;
+        int jumlahObat1, jumlahObat2, jumlahObat3, jumlahObat4, jumlahObat5;
         
         public void setNama(String n){
             nama=n;
@@ -13,5 +14,34 @@ public class Apotek {
         public void setAlamat(String a){
             alamat=a;
         }
+        
+        public void obat1(int jumlah){
+            System.out.println("Jumlah Obat : "+jumlah);
+            jumlahObat1 = jumlah*3000;
+            System.out.println("Harganya : "+jumlahObat1);
+        }
+        public void obat2(int jumlah){
+            System.out.println("Jumlah Obat : "+jumlah);
+            jumlahObat2 = jumlah*35000;
+            System.out.println("Harganya : "+jumlahObat2);
+        }
+        public void obat3(int jumlah){
+            System.out.println("Jumlah Obat : "+jumlah);
+            jumlahObat2 = jumlah*25000;
+            System.out.println("Harganya : "+jumlahObat2);
+        }
+        public void obat4(int jumlah){
+            System.out.println("Jumlah Obat : "+jumlah);
+            jumlahObat2 = jumlah*5000;
+            System.out.println("Harganya : "+jumlahObat2);
+        }
+        public void obat5(int jumlah){
+            System.out.println("Jumlah Obat : "+jumlah);
+            jumlahObat2 = jumlah*10000;
+            System.out.println("Harganya : "+jumlahObat2);
+        }
+        public void jumlahObat(int a){
+            int Total = jumlahObat1+jumlahObat2+jumlahObat3+jumlahObat4+jumlahObat5;
+            System.out.println("Total Harga : "+Total);
+        }
 }
-
