4629814fbde02430021ad3ba1e0e4552cc6c7847
Nofry <nofryarman@gmail.com>
2016-03-22 20:05:03 -0700

    membuat variabel global dan constructor (overloading)

--- src/tugas/bab3/TugasBab3.java
+++ src/tugas/bab3/TugasBab3.java
@@ -1,19 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugas.bab3;
+class Apotek {
+    public String nama;
+     public String alamat;
+     public String obat[];
+     public int harga[];
+     public String beliObat;
+     public int jumlah, hargaTotal;
+     public Apotek(String[] obat, int[] harga){
+         this.obat = obat;
+         this.harga = harga;
+     }
+     public Apotek(String nama, String alamat){
+         this.nama = nama;
+         this.alamat = alamat;
+     }
+     
+     
+}
 
-/**
- *
- * @author irwin deriyan
- */
+
+
+
+
+
 public class TugasBab3 {
+    
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
