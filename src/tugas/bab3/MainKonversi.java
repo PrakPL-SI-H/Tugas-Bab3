918883642835812a8d75b9742f031d2a8f0c29f6
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-23 20:01:32 +0700

    edit  main class

---
+++ src/tugas/bab3/MainKonversi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MainKonversi {
+	public static void main(String[] args) {
+		Konversi Angka = new Konversi();
+                String s;
+                int a;
+                Scanner in = new Scanner(System.in);
+             do{
+                System.out.println("Menu");
+                System.out.println("m");
+                System.out.println("m");
+                System.out.println("mask");
+                a = in.nextInt();
+                switch(a){
+            case 1:
+                
+            case 2 :
+                
+           }while(a  != 0 && !s.equalsIgnoreCase("n"));
+            System.out.println("bye.....byeeee");
+        }
+}
