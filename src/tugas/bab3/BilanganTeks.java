762b563fd6c3e5c7605b3171b66c19da7db76bc7
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-23 19:46:54 +0700

    menambah array teks

--- src/tugas/bab3/BilanganTeks.java
+++ src/tugas/bab3/BilanganTeks.java
@@ -1,8 +1,7 @@
 package prakproglan;
 public class BilanganTeks {
 
-    private final String angka[] = {
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
-    
-}
+    private final String angka[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
+    private final String teks[] = {"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas" };
 
+}
