959581a0051f6330e56513d92efec682f4ceb517
arifardhan <ariframadhan902@yahoo.com>
2016-03-23 21:30:06 +0700

    Membuat method overloading dan overloading konstruktor

--- src/tugas/bab3/Apotik.java
+++ src/tugas/bab3/Apotik.java
@@ -25,4 +25,32 @@ public class Apotik {
         System.out.println("====================================");
     }
 
+    public void id(String nama, String alamat) {
+        System.out.println("Patient name          : " + nama);
+        System.out.println("Patient address       : " + alamat);
+    }
+
+    public void daftarHarga(String[] obat, int[] jumlah) {
+        int i, j, k;
+        int l = 0;
+        int TOTAL = 0;
+        System.out.println("DRUG \t\t QTY \t PRICE \t TOTAL");
+        System.out.println("================================");
+        for (i = 0; i < obat.length; i++, l++) {
+            if (jumlah[i] == 0) {
+                continue;
+            }
+            for (k = 0; k < obat.length; k++) {
+                if (obat[l].equalsIgnoreCase(this.obat[k])) {
+                    int TOT = jumlah[i] * harga[k];
+                    System.out.printf("%s\t\t %d\t %d\t %d", this.obat[k], jumlah[i], harga[k], TOT);
+                    TOTAL += TOT;
+                }
+            }
+        }
+        System.out.println("\n" + "==================");
+        System.out.println("\t\t\t\t " + TOTAL);
+        System.out.println("==================");
+    }
+
 }
