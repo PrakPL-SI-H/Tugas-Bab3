1ae120863d7a2b77ebaee3e6120dfbcac9872bb7
istighfarinba <bafarcorp@gmail.com>
2016-03-23 18:04:04 +0700

    penambahan variabel dan method baru(outputan)

--- src/ProsesKonversi.java
+++ src/ProsesKonversi.java
@@ -2,5 +2,16 @@ package bab3konversiangka;
 
 public class ProsesKonversi {
 
+    String valueword;
+    String valuenum;
 
+    public void outputword() {
+        System.out.println("Maka Hasil Konversinya : " + valueword);
+
+    }
+
+    public void outputnum() {
+        System.out.println("Maka Hasil Konversinya : " + valuenum);
+
+    }
 }
