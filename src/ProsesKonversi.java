d77025b1daef70af6a002c00b3f5c16338a057fd
istighfarinba <bafarcorp@gmail.com>
2016-03-23 18:09:14 +0700

    pembuatan overloading method dan logika angka ke huruf

--- src/ProsesKonversi.java
+++ src/ProsesKonversi.java
@@ -13,10 +13,33 @@ public class ProsesKonversi {
     public void outputnum() {
         System.out.println("Maka Hasil Konversinya : " + valuenum);
 
-    }//array untuk word angka normal dan angka normal
+    }
     String[] normalword = {"Nol", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam",
         "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"};
     String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
 
+    public String overloadingMeth(int angka) {
+        int x;
+        x = angka % 10;
+        valueword = "";
+        if (angka == 0) {
+            valueword = "Nol";
 
+        } else if (angka == 100) {
+            valueword = "Seratus";
+        } else if (x == 0) {
+            valueword += normalword[angka / 10] + " Puluh";
+
+        } else if (angka < 12) {
+            valueword += normalword[angka];
+        } else if (angka < 20) {
+
+            valueword += normalword[angka - 10] + " Belas";
+        } else if (angka < 100) {
+            valueword += normalword[angka / 10] + " Puluh " + normalword[angka % 10];
+        } else if (angka > 100) {
+            System.out.println("MASUKAN ERROR,ANGKA TIDAK BOLEH LEBIH DARI 100");
+        }
+        return valueword;
+    }
 }
