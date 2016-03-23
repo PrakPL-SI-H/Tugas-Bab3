5f2659ad82536aab0af9497d6998f10334115c92
istighfarinba <bafarcorp@gmail.com>
2016-03-23 18:07:38 +0700

    revisi class..array untuk nama angka normal dan angka normal

--- src/ProsesKonversi.java
+++ src/ProsesKonversi.java
@@ -13,7 +13,7 @@ public class ProsesKonversi {
     public void outputnum() {
         System.out.println("Maka Hasil Konversinya : " + valuenum);
 
-    }
+    }//array untuk word angka normal dan angka normal
     String[] normalword = {"Nol", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam",
         "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"};
     String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
