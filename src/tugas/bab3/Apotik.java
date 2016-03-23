0850dc7aad22eca791acabf7ef2ca71140382ce3
arifardhan <ariframadhan902@yahoo.com>
2016-03-23 21:23:34 +0700

    Membuat Atribute di class, dan membuat variabel array pada main class

--- src/tugas/bab3/Apotik.java
+++ src/tugas/bab3/Apotik.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Praktikum3;
 
-/**
- *
- * @author Arif Ramadhan
- */
 public class Apotik {
-    
+
+    private String nama;
+    private String alamat;
+    private String[] obat;
+    private int[] jumlah;
+    private int[] harga;
+
+    public Apotik() {
+    }
+
+
 }
+
--- src/tugas/bab3/ApotikMain.java
+++ src/tugas/bab3/ApotikMain.java
@@ -1,5 +1,15 @@
 package Praktikum3;
 
+import java.util.Scanner;
+
 public class ApotikMain {
 
+    static Scanner in = new Scanner(System.in);
+    static String[] obat = {"Paramex", "Super Tetra", "Promagh", "Fatigon", "Panadol"};
+    static String[] inputObat = new String[4];
+    static String nama, alamat;
+    static int[] jumlah = new int[5];
+    static int[] harga = {4000, 15000, 5000, 5000, 12000};
+    
+    
 }
