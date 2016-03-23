fbfb5d1b02deec46ce387d91ba10fb403580a33a
Johan Fajar <johanrubik@gmail.com>
2016-03-23 20:02:31 +0700

    Edit dataapotek 2

--- src/tugas/bab3/mainApotek/src/mainapotek/DataApotek.java
+++ src/tugas/bab3/mainApotek/src/mainapotek/DataApotek.java
@@ -21,7 +21,7 @@ public class DataApotek {
         do {
             System.out.println("----------------------------------------------");
             System.out.println("|   No    Kode    Nama Obat         Harga    |");
-            System.out.println("|   1.    KO123   Obat Gata         Rp. 5000 |");
+            System.out.println("|   1.    KO123   Obat Gatal        Rp. 5000 |");
             System.out.println("|   2.    KO990   Obat Sakit Kepala Rp. 3500 |");
             System.out.println("|   3.    KO784   Obat Sakit Gigi   Rp. 3000 |");
             System.out.println("|   4.    KO226   Obat Demam        RP. 2800 |");
@@ -32,10 +32,34 @@ public class DataApotek {
             System.out.print("Masukkan pilihan anda : ");
             pilih = s.nextInt();
             switch (pilih){
-                case 1;
+                case 1: no++;
+                        obat[no] = new Apotek("KO123", "Obat Gatal", 5000);
+                        DH += 5000;
+                        break;
+                case 2: no++;
+                        obat[no] = new Apotek("KO990", "Obat Sakit Kepala", 3500);
+                        DH += 3500;
+                        break;
+                case 3: no++;
+                        obat[no] = new Apotek("KO784", "Obat Sakit Gigi", 3000);
+                        DH += 3000;
+                        break;
+                case 4: no++;
+                        obat[no] = new Apotek("KO226", "Obat Demam", 2800);
+                        DH += 2800;
+                        break;
+                case 5: no++;
+                        obat[no] = new Apotek("KO888", "Obat Batuk", 4900);
+                        DH += 4900;
+                        break;
+                case 6: no++;
+                        obat[no] = new Apotek("KO992", "Obat Flu", 2500);
+                        DH += 2500;
+                        break;
+                case 0: break;
+                default: System.out.println("Pilihan tidak ada! Pastikan memilih sesuai menu");               
             }
-        } 
-        
-        
+        } while (pilih!=0);       
     }
+    
 }
