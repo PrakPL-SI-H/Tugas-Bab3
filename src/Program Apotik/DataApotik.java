4a0fcd5f45559663f2b53add24de16817aa6f3c1
istighfarinba <bafarcorp@gmail.com>
2016-03-23 18:28:29 +0700

    pembuatan overloading konstruktor

--- src/Program Apotik/DataApotik.java
+++ src/Program Apotik/DataApotik.java
@@ -7,6 +7,20 @@ public class DataApotik {
     public String[] list, pilObat;
     public int jum, total;
 
-   
+    public DataApotik() {
+
+    }
+
+    public DataApotik(String a, String b) {
+        this.user = a;
+        this.alamat = b;
+    }
+
+    public DataApotik(String[] a, int[] b) {
+        this.list = a;
+        this.harga = b;
+    }
+
+    
 
 }
