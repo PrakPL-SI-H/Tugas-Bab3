dbe2e702641ac92febf8383bc0a10b140cf32a3b
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-23 21:26:21 +0700

    edit main class soal no2

--- src/tugas/bab3/MainApotik.java
+++ src/tugas/bab3/MainApotik.java
@@ -10,13 +10,19 @@ public class MainApotik {
         obat.Nama = ini.nextLine();
         System.out.println("Alamat = ");
         obat.Alamat = ini.nextLine();
-
+        do{
         System.out.println("Menu");
         System.out.println("1.Pilih Obat");
         System.out.println("2.Total Harga");
         System.out.println("9.keluar");
         System.out.println("Masukkan Urutan = ");
         i = ini.nextInt();
-
+        switch (i){
+            case 1 : 
+            case 2 : 
+        }
+    }while(i    !=0);
+        System.out.println("");
+        
    }
 }
