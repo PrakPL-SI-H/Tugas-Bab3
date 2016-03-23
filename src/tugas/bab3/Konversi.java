40783c1f2a29ee14faeb696975fee74996021169
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-03-23 22:06:22 +0700

    percobaan 1 class konversi

---
+++ src/tugas/bab3/Konversi.java
@@ -0,0 +1,16 @@
+package program_Laporan;
+
+public class Konversi {
+
+    private final String[] angka = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
+    private String nilaiAngka;
+
+    private final String[] huruf = {"Nol", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam",
+        "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"};
+    private String nilaiHuruf;
+
+    public String getHasilAngka() {
+        return nilaiAngka;
+    }
+
+}
