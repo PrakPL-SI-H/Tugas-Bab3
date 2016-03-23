5709a72e21914cbcf735250f918c6d67ccc051b8
FelixYosa <felix_yosa@ymail.com>
2016-03-23 19:53:02 +0700

    angka jadi huruf

--- src/tugas/bab3/ClassBilangan.java
+++ src/tugas/bab3/ClassBilangan.java
@@ -1,9 +1,16 @@
 package hurufbilangan;
 
 public class ClassBilangan {
-
     public String rubah(int b) {
         String a[] = {"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"};
-
+        String huruf = null;
+        if (b < 12) {
+            huruf = ("" + a[b]);
+        } else if (b < 20) {
+            huruf = (rubah(b - 10) + " belas ");
+        } else if (b < 100) {
+            huruf = (rubah(b / 10) + " puluh " + rubah(b % 10));
+        }
+        return huruf;
     }
 }
