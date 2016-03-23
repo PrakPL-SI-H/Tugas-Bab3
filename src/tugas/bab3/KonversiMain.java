8017d6f65cacff166eb00b9f55c634c120a14650
arifardhan <ariframadhan902@yahoo.com>
2016-03-23 13:36:42 +0700

    Membuat Konversi angka ke bilangan (inputan user) pada main class

--- src/tugas/bab3/KonversiMain.java
+++ src/tugas/bab3/KonversiMain.java
@@ -10,8 +10,16 @@ public class KonversiMain {
         System.out.println("KONVERSI BILANGAN-ANGKA (satu-seratus)");
         System.out.println("======================================");
         System.out.print("Masukkan bilangan : ");
-        String y = in.nextLine();
-        konversi.showAngka(y);
+        String b = in.nextLine();
+        konversi.showAngka(b);
+        System.out.println("");
+        
+        System.out.println("KONVERSI ANGKA-BILANGAN (1-100)");
+        System.out.println("======================================");
+        System.out.print("Masukkan bilangan : ");
+        int a = in.nextInt();
+        konversi.showBilangan(a);
 
+        
     }
 }
