c8536d912cb4871e654ac9f7a5aa4c5b1691d8e4
FelixYosa <felix_yosa@ymail.com>
2016-03-23 22:04:51 +0700

    buat class obat

---
+++ src/tugas/bab3/Obat.java
@@ -0,0 +1,7 @@
+package mainapotek;
+
+public class Obat {
+    public int kode[] = {1, 2, 3, 4, 5};
+    public String obat[] = {"Obat 1", "Obat 2", "Obat 3", "Obat 4", "Obat 5"};
+    public int uang[] = {3500, 5500, 3000, 4500, 2500};
+}
