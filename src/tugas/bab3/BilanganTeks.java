4fbf26cb0b7e4d97f3a1682eb3658f36b8d17ab2
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-23 22:15:18 +0700

    menambah method getangbil,get bilang, dan overloadingmeth dengan parameter integer

--- src/tugas/bab3/BilanganTeks.java
+++ src/tugas/bab3/BilanganTeks.java
@@ -4,7 +4,26 @@ public class BilanganTeks {
     private final String angka[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
     private final String teks[] = {"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas" };
     private static String angbil;
-       
+    private static String bilang;
+    
+    public String overloadingMeth(String b) {
+    for (int i = 0; i < teks.length; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (b.equalsIgnoreCase(teks[i])) {
+                bilang = angka[i];                }
+            else if (b.equalsIgnoreCase(teks[i] + " belas")) {
+                bilang = angka[1] + angka[i];    }
+            else if (b.equalsIgnoreCase(teks[i] + " puluh")) {
+                bilang = angka[i] + angka[0];    }
+            else if (b.equalsIgnoreCase(teks[i] + " puluh " + teks[j])) {
+                bilang = angka[i] + angka[j];  } 
+            else if (b.equalsIgnoreCase("seratus")) {
+                bilang = "100";   
+            }  
+        }  
+    }
+        return bilang;   
+    }
     public String overloadingMeth(int t){
         angbil="";
         if(t<12){
@@ -19,4 +38,13 @@ public class BilanganTeks {
             System.out.println("Nilai tidak ditemukan"); }
         return angbil;
     }
+    
+    public String getAngBil(){
+        return angbil;
+    }
+    
+    public String getBilAng(){
+        return bilang;
+    }
+
 }
