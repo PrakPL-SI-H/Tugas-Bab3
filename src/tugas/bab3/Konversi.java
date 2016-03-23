29679b2e318bc48f0f993743ef4e04878212dd92
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-03-23 22:09:35 +0700

    percobaan final class konversi

--- src/tugas/bab3/Konversi.java
+++ src/tugas/bab3/Konversi.java
@@ -28,5 +28,41 @@ public class Konversi {
             System.err.println("Angka tidak boleh lebih dari 100,dan angka anda melebihi 100 ");
         }
         return nilaiHuruf;
+        
+    }
+   public String overLoadingMeth(String huruf) {
+        for (int i = 0; i < this.huruf.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (huruf.equalsIgnoreCase(this.huruf[i])) {
+                    nilaiAngka = angka[i];
+                } else if (huruf.equalsIgnoreCase(this.huruf[i])) {
+                    nilaiAngka = angka[i];
+                } else if (huruf.equalsIgnoreCase(this.huruf[i] + " belas")) {
+                    nilaiAngka = angka[1] + angka[i];
+                } else if (huruf.equalsIgnoreCase(this.huruf[i] + " puluh")) {
+                    nilaiAngka = angka[i] + angka[0];
+                } else if (huruf.equalsIgnoreCase(this.huruf[i] + " puluh " + this.huruf[j])) {
+                    nilaiAngka = angka[i] + angka[j];
+                } else if (huruf.equalsIgnoreCase("Seratus")) {
+                    nilaiAngka = angka[1] + angka[0] + angka[0];
+                }
+            }
+        }
+        return nilaiAngka;
+    }
+
+    public String getHasilHuruf() {
+        return nilaiHuruf;
+    }
+
+    public static void menu() {
+        System.out.println("==========||||||||||==========||||||||||==========||||||||||==========||||||||||==========");
+        System.out.println(" ===================------ PROGRAM KONVERSI DARI ANGKA KE HURUF ------==================== ");
+        System.out.println(" ===================------ PROGRAM KONVERSI DARI HURUF KE ANGKA ------==================== ");
+        
+        System.out.println("1. Konversi dari bentuk angka ke bentuk huruf : ");
+        System.out.println("2. Konversi dari bentuk huruf ke bentuk angka : ");
+        System.out.println("3. Keluar ");
+        System.out.print("Masukkan pilihan anda : ");
     }
 }
