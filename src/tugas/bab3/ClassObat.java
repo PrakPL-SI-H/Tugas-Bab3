6c01133e7772a61e2228c504a6bc5ac07985d696
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-03-23 22:15:42 +0700

    percobaan pertama class obat tanpa method

---
+++ src/tugas/bab3/ClassObat.java
@@ -0,0 +1,16 @@
+package program_Laporan;
+import java.util.Scanner;
+
+public class ClassObat {
+     Scanner in = new Scanner(System.in);
+     int totalObat1,totalObat2,totalObat3,totalObat4;
+     
+       public void totalAll(int jumlahsemua){
+          
+         int TotalSemua = totalObat1+totalObat2+totalObat3+totalObat4;
+           System.out.println("total harganya adalah ="+TotalSemua); 
+           
+       }
+
+}
+
