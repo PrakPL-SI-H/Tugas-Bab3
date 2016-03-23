3de0d9a8d20e39ad3e9ee55c2f508627f821f8b3
FelixYosa <felix_yosa@ymail.com>
2016-03-23 19:44:51 +0700

    buat class

---
+++ src/tugas/bab3/ClassBilangan.java
@@ -0,0 +1,9 @@
+package hurufbilangan;
+
+public class ClassBilangan {
+
+    public String rubah(int b) {
+        String a[] = {"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"};
+
+    }
+}
