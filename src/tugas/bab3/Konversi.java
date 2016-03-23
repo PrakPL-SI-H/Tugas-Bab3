4008f1428a1a0b5113f1909f705dbca6b13340a2
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-23 11:09:21 +0700

    edit method class

---
+++ src/tugas/bab3/Konversi.java
@@ -0,0 +1,5 @@
+class  Konversi{	
+	String angka(int satuan){
+		String []huruf ={"Nol","Satu","Dua","Tiga","Empat","Lima","Enam","Tujuh","Delapan","Sebilan","Sepuluh","Sebelas"};
+		String hasil="";
+
