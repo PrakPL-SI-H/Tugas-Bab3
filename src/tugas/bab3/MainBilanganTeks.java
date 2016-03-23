d2ed7b991d47e48b58e33de063518ed705f8bc93
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-23 22:15:56 +0700

    menambahkan switch case

--- src/tugas/bab3/MainBilanganTeks.java
+++ src/tugas/bab3/MainBilanganTeks.java
@@ -9,6 +9,24 @@ public class MainBilanganTeks {
         System.out.println("2. Mengubah Teks Menjadi Bilangan");
         System.out.print("Masukkan Pilihan : ");
         int pilihan = in.nextInt();
-        
+        switch(pilihan){
+            case 1 :
+                System.out.println("=================================");
+                System.out.println("Mengubah Bilangan Menjadi Teks");
+                System.out.print("Masukkan Bilangan : ");
+                bete.overloadingMeth(in.nextInt());
+                System.out.println("Output : "+bete.getAngBil());
+                System.out.println("=================================");
+                break;
+            case 2 :
+                System.out.println("=================================");
+                System.out.println("Mengubah Teks Menjadi Bilangan");
+                System.out.print("Masukkan Teks : ");
+                bete.overloadingMeth(in.next());
+                bete.overloadingMeth(in.next());
+                System.out.println("Output : "+bete.getBilAng());
+                System.out.println("=================================");
+                break;
+        }
     }
 }
