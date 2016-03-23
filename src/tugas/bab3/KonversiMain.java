33f632f4e9bf3180f4a14b3bef01a957c781fae5
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-03-23 22:13:59 +0700

    perobaan konversimain final dengan case

--- src/tugas/bab3/KonversiMain.java
+++ src/tugas/bab3/KonversiMain.java
@@ -14,7 +14,19 @@ public class KonversiMain {
             Scanner in = new Scanner(System.in);
             pilihan = in.nextInt();
             switch (pilihan) {
-                
+                  case 1:
+                    System.out.print("Masukkan angka yang ingin anda ubah : ");
+                    ubah.overloadingMeth(in.nextInt());
+                    System.out.println(ubah.getHasilHuruf());
+                    break;
+                case 2:
+                    System.out.print("Masukkan huruf yang ingin anda ubah : ");
+                    ubah.overLoadingMeth(in.nextLine());
+                    ubah.overLoadingMeth(in.nextLine());
+                    System.out.println(ubah.getHasilAngka());
+                    break;
+                case 3:
+                    break;
             }
             System.out.print("Ingin melakukan pengkonversian ulang? (y/n) : ");
             s = in.next();
