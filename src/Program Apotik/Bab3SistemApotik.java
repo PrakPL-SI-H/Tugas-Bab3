0ba6910d0a5112fe23b176fec010efe6d8b5cea4
istighfarinba <bafarcorp@gmail.com>
2016-03-23 18:24:56 +0700

    pembuatan main class Program Apotik

---
+++ src/Program Apotik/Bab3SistemApotik.java
@@ -0,0 +1,17 @@
+package bab3sistemapotik;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Istighfarin Bahtiar AMry
+ * @NIM 155150401111129
+ * @Kelas SI-H
+ */
+public class Bab3SistemApotik {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+
+}}
