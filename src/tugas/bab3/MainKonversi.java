890dbb037bf5245651956fc02073e9fd0b4c1800
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-23 23:10:54 +0700

    edit main class  no 2 final

--- src/tugas/bab3/MainKonversi.java
+++ src/tugas/bab3/MainKonversi.java
@@ -7,16 +7,26 @@ public class MainKonversi {
                 Scanner in = new Scanner(System.in);
              do{
                 System.out.println("Menu");
-                System.out.println("m");
-                System.out.println("m");
-                System.out.println("mask");
+                System.out.println("1.Konversi Angka ke Huruf");
+                System.out.println("2.Konversi Huruf ke Angka");
+                System.out.print("Masuk pilihan = ");
                 a = in.nextInt();
                 switch(a){
-            case 1:
-                
-            case 2 :
-                
+                case 1:
+                    System.out.print("Masukkan angka anda : ");
+                    Angka.overloadingMeth(in.nextInt());
+                    System.out.println(Angka.getHasilHuruf());
+                    break;
+                case 2:
+                    System.out.print("Masukkan huruf anda : ");
+                    Angka.overLoadingMeth(in.nextLine());
+                    Angka.overLoadingMeth(in.nextLine());
+                    System.out.println(Angka.getHasilAngka());
+                    break;
+                }
+                System.out.print("Ingin konversi lagi? (y/n) : ");
+                s = in.next();
            }while(a  != 0 && !s.equalsIgnoreCase("n"));
-            System.out.println("bye.....byeeee");
+            System.out.println("Terima Kasih Sudah Mencoba");
         }
 }
