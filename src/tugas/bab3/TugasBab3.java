070bf2e2dba4c9b8a0d146b2292df1de1475a8ce
rodhirf <rodhirobb@gmail.com>
2016-03-23 07:48:06 +0700

    membuat kelas
    membuat kelas PraktikumBab3 dan deklarasi atribut

--- src/tugas/bab3/TugasBab3.java
+++ src/tugas/bab3/TugasBab3.java
@@ -1,21 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tugas.bab3;
+import java.util.Scanner;
 
-/**
- *
- * @author irwin deriyan
- */
-public class TugasBab3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+public class PraktikumBab3 {
+int satuan, hasilsatuan;
+String bilangan, hasilbilangan;
