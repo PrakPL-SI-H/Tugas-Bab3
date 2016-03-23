e6da71e12cc19a9994e34f37d9f3c0069325237b
dyahas <arifiani.dyah@gmail.com>
2016-03-23 18:54:37 +0700

    Class Main

--- src/LatihanBab3.java
+++ src/LatihanBab3.java
@@ -1,17 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
+package mainapotek;
+
+import java.util.Scanner;
+
+public class MainApotek {
+
+    public static int code[], data[];
+
     public static void main(String[] args) {
-        System.out.println("tess");
-        
-        
-    }
-    
-}
+        Scanner in = new Scanner(System.in);
+        System.out.println("========= SISTEM INFORMASI PENJULAN APOTEK X ========");
+        System.out.print("Masukkan Nama Anda         : ");
+        String nama = in.next();
+        System.out.print("Masukkan Alamat Anda       : ");
+        String alamat = in.next();
+        apotek x = new apotek(nama, alamat);
+        System.out.print("Masukkan Jumlah Jenis Obat : ");
+        int jumlah = in.nextInt();
+        System.out.println("=====================================================");
+        System.out.println("                 Daftar Obat  ");
+        System.out.println("=====================================================");
+        System.out.println("1.  Ampicilin         : Rp   8500,-");
+        System.out.println("2.  Antalgin          : Rp   5000,-");
+        System.out.println("3.  Antimo Anak       : Rp   2000,-");
+        System.out.println("4.  Betadine          : Rp   9000,-");
+        System.out.println("5.  Bisolvon          : Rp   7500,-");
+        System.out.println("6.  Bodrex            : Rp   5000,-");
+        System.out.println("7.  Cefadroxil        : Rp   9000,-");
+        System.out.println("8.  Decolgen          : Rp   2500,-");
+        System.out.println("9.  GG                : Rp   3000,-");
+        System.out.println("10. Paracetamol       : Rp   5000,-");
+        System.out.println("11. Paramex           : Rp   2500,-");
+        System.out.println("12. Rhinos            : Rp 30.000,-");
+        System.out.println("=====================================================");
+        System.out.println("Pilih Obat            : ");
