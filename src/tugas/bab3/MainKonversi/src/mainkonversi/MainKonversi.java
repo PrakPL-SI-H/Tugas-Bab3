0749cdd867480df894451eaf4d93808d98a50497
Johan Fajar <johanrubik@gmail.com>
2016-03-23 11:04:30 +0700

    membuat atribut

---
+++ src/tugas/bab3/MainKonversi/src/mainkonversi/MainKonversi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainkonversi;
+
+/**
+ *
+ * @author ASUS-A455L
+ */
+public class MainKonversi {
+    String Bilangan;
+    int Angka;
+    String [] x;
+    static String [] angkabilangan = {"nol","satu","dua","tiga","empat","lima","enam","tujh","delapan","sembilan","sepuluh","sebelas"};
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
