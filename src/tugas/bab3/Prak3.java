0084bad35fc78a457c859e7e4d61bc37b22902e1
wenigustirahayu <wenigr97@gmail.com>
2016-03-23 18:30:05 +0700

    edit main class overloadingmeth

---
+++ src/tugas/bab3/Prak3.java
@@ -0,0 +1,9 @@
+package prak3;
+import java.util.Scanner;
+public class Prak3 {
+public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        Scanner in2 = new Scanner (System.in);
+        
+    }
+}	
