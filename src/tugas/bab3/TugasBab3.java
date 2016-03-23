7675d675e3efa09792b9633106fe628eae7dc777
Nofry <nofryarman@gmail.com>
2016-03-22 20:16:19 -0700

    buat menu menggunakan switch case di main class

--- src/tugas/bab3/TugasBab3.java
+++ src/tugas/bab3/TugasBab3.java
@@ -18,7 +18,7 @@ class Apotek {
          this.alamat = alamat;
      }
      public void daftarObat(int i){
-         System.out.println(obat[i]+"  "+harga[i]);
+         System.out.print(obat[i]+"  "+harga[i]);
      }
      public void daftarObat(String pilih, int jumlah){
          
@@ -36,14 +36,33 @@ class Apotek {
 public class TugasBab3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int pilihan;
         String[] daftar = {"ObatA","ObatB","ObatC"};
         int Harga[] ={10000,12000,13000};
         System.out.print("Masukkan Nama   : ");
-        String nama = in.next();
+            String nama = in.next();
         System.out.print("Masukkan Alamat : ");
-        String alamat = in.next();
+            String alamat = in.next();
         Apotek user = new Apotek(nama, alamat);
         Apotek obat = new Apotek(daftar, Harga);
+        System.out.println("=Menu Sistem Informasi Apotek=");
+        System.out.println("1. Daftar Obat");
+        System.out.println("2. Pilihan Obat");
+        System.out.println("3. Kuitansi Obat");
+        System.out.println("4. Exit");
+        System.out.print("Masukan Pilihan : ");
+            pilihan = in.nextInt();
+            switch(pilihan){
+                case 1:
+                    System.out.println("=====Daftar Obat=====");
+                    for (int i = 0; i < daftar.length; i++) {
+                       obat.daftarObat(i);
+                       System.out.println();
+                    }
+                    break;
+                case 2:
+                    
+            }
     }
     
 }
