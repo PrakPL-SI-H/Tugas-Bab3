72737530c202b504b6caa30e159a401388031c8f
Rasyid <mrasyid1@yahoo.com>
2016-03-23 21:44:55 -0700

    Pengisian data mahasiswa. Pembuatan overoading method untuk konversi dari int menjadi string.

--- src/LatihanBab3.java
+++ src/LatihanBab3.java
@@ -1,13 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
+import java.util.Scanner;
+
+class Conversion{
+
+
+ String Var[] = {"","Satu","Dua","Tiga","Empat","Lima","Enam","Tujuh",
+                 "Delapan","Sembilan","Sepuluh","Sebelas","Seratus","nol"};
+ 
+ 
+ public void NrOverload ( int X ){
+        if (X == 0){
+             System.out.println("Hasil Konversi : "+Var[13]);
+         }else 
+          if (X == 100){
+          System.out.println("Hasil Konversi : "+100);
+         }else
+          if ( X < 12){
+          System.out.println("Hasil Konversi : "+Var[X]);
+         }else 
+          if ( X >=12 && X <=19){
+          System.out.println("Hasil Konversi : "+(Var[X%10])+" Belas");
+         }else
+          if ( X>=20 && X<100){
+          System.out.println("Hasil Konversi : "+(Var[X/10])+" Puluh "+(Var[X%10]));
+         }else{ System.out.println("Masukkan Bilangan Antara 0-100!");
+         }   
+    }   
+
+}
+
+
+public class LatihanBab3 {
     public static void main(String[] args) {
         System.out.println("tess");
         
@@ -15,3 +37,4 @@ public class LatihanBab1 {
     }
     
 }
+
