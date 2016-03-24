eaef526fb99007e61a5e7027f46d9a4db29a2b6a
Rasyid <mrasyid1@yahoo.com>
2016-03-23 22:00:14 -0700

    Pembuatan method NrOverloading untuk mengonversi dari tipe data string menjadi int.

--- src/LatihanBab3.java
+++ src/LatihanBab3.java
@@ -1,31 +1,54 @@
 import java.util.Scanner;
-
+//Class Conversion untuk mengkoversi dari string ke int maupun sebaliknya
 class Conversion{
 
-
+//inisialisasi isi dari array var
  String Var[] = {"","Satu","Dua","Tiga","Empat","Lima","Enam","Tujuh",
                  "Delapan","Sembilan","Sepuluh","Sebelas","Seratus","nol"};
  
- 
+ //overoading method untuk konversi dari tipe data int ke tipe data string
  public void NrOverload ( int X ){
         if (X == 0){
-             System.out.println("Hasil Konversi : "+Var[13]);
-         }else 
-          if (X == 100){
-          System.out.println("Hasil Konversi : "+100);
-         }else
-          if ( X < 12){
-          System.out.println("Hasil Konversi : "+Var[X]);
-         }else 
-          if ( X >=12 && X <=19){
-          System.out.println("Hasil Konversi : "+(Var[X%10])+" Belas");
-         }else
-          if ( X>=20 && X<100){
-          System.out.println("Hasil Konversi : "+(Var[X/10])+" Puluh "+(Var[X%10]));
-         }else{ System.out.println("Masukkan Bilangan Antara 0-100!");
-         }   
+        System.out.println("Hasil Konversi : "+Var[13]);
+        }else 
+        if (X == 100){
+        System.out.println("Hasil Konversi : "+100);
+        }else
+        if ( X < 12){
+        System.out.println("Hasil Konversi : "+Var[X]);
+        }else 
+        if ( X >=12 && X <=19){
+        System.out.println("Hasil Konversi : "+(Var[X%10])+" Belas");
+        }else
+        if ( X>=20 && X<100){
+        System.out.println("Hasil Konversi : "+(Var[X/10])+" Puluh "+(Var[X%10]));
+        }else{ System.out.println("Masukkan Bilangan Antara 0-100!");
+        }   
     }   
-
+    //overloading method untuk konversi dari tipe ddata string ke intejer
+    public void NrOverload ( String X ){
+       for (int i =0 ; i<Var.length ; i++){
+       if (X.equals(Var[12])){
+       System.out.println("Hasil Konversi : "+100); 
+       break;
+       }else 
+       if (X.equalsIgnoreCase(Var[i])){
+       System.out.println("Hasil Konversi : "+i);
+       }else 
+       if (X.equalsIgnoreCase(Var[i] +" Belas")){
+       System.out.println("Hasil Konversi : "+(i+10)); 
+       }else
+       if (X.equalsIgnoreCase(Var[i] +" Puluh")){
+       System.out.println("Hasil Konversi : "+(i*10));   
+       }
+       for (int j = 0 ; j<Var.length ; j++){
+       if (X.equals(Var[i] + " Puluh " + Var[j])){
+       System.out.println("Hasil Konversi : "+(i)+(j));
+           }
+       }
+      } 
+    
+    }
 }
 
 
